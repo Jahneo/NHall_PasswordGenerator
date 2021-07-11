d8d6ebb6556b2772dee0c62c9b5f8cb6aa05746b
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-07-11 04:52:43 -0400

    edit README.md try writing pseudo codes and experimenting with different codes change name of web-site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // Assignment code here
 // Passwords contains alpha numerical values and symbols
 
-var generateBtn = document.querySelector("#generate");
 
-// declare variables letters numbers and symbols
+
+// declare variables letters numbers and symbols as arrays to generate and store password
+
 var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-// applying  str.toUpperCase() to include upper case characters
 var lowCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var symbols = ['!', '@', '#','$','%','^','&','*','-','+','='];
@@ -13,86 +13,17 @@ var symbols = ['!', '@', '#','$','%','^','&','*','-','+','='];
 
 // Get references to the #generate element
 
+var generateBtn = document.querySelector("#generate");
+
 // create function to ask about password needed and test validity of options
+// prompt user for length of password
+//Give users the option to choose the characters numbers etc they need in the password
+//carry out test to generate a secure password contain leters and numbers and special character
+//Psuedo Code
+// prompt confirm do while and if else statements
+//check values and prompt is criteria should match secure password
 
-
-     function Pass() {
-        var PassQ = false;
-    do{
-	
-	    var length = prompt("Choose the length of the password you need it should be between 8 to 32 in length");
     
-	    var letters = confirm("Should your password have upper letters?");
-        var lowCase = confirm("Should your password have lower letters?");
-        var numbers = confirm("Should your password have numbers?");
-        var symbols = confirm("Should your password have symbols?");
-
-        var getPrompt = {
-            length: length,  
-            letters: letters,
-            lowCase: lowCase,
-            numbers: numbers,
-            symbols: symbols
-            
-        }
-    if ((length < 8)||(length > 32)) 
-        alert("Choose a number between 8 and 32 to get a secure password");
-      
-     else if(((!letters)&&(!numbers)&&(!symbols)&&(!lowCase))||(!numbers)&&(!symbols)||(!numbers)&&(!letters))
-        alert("Secure password must have at least a letter and number or a symbol and number..Make a choice!!!...");
-        else
-        PassQ = true;
-
-    } while(!PassQ);
-    return getPrompt;
-}
-  //document.getElementById("demo").innerHTML = "Hello World!";
-
-  function generatePassword() {
-
-        var passwordOption = Pass();
-        var possibleCombo = [];
-        var finalPassword = [];
-  
-
-  if(passwordOption.numbers) {
-      for (var i of numbers)
-      possibleCombo.push(i);
-  }
-  
-
-  if(passwordOption.letters) {
-    
-    for (var i of letters)
-    
-    possibleCombo.push(i);
-
-}
-
-if(passwordOption.lowCase) {
-    
-    for (var i of lowCase)
-    possibleCombo.push(i);
-    
-
-}
-if(passwordOption.symbols) {
-    for (var i of symbols)
-    possibleCombo.push(i);
-}
-
-console.log(possibleCombo);
-
-
-for (var i = 0; i < passwordOption.length; i++) {
-
-    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
-
-
-}
-console.log(finalPassword);
-return finalPassword;
- }
 
 
   
@@ -101,10 +32,8 @@ return finalPassword;
 
     function writePassword() {
      var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    //var passwordText = document.getElementById("#password")
-
-  passwordText.value = password;
+     var passwordText = document.querySelector("#password");
+     passwordText.value = password;
 
 }
 
